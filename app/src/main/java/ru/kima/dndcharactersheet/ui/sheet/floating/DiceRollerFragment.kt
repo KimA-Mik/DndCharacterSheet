aba6f9b546a88c5b425a315f8be184e0d04bc2be
KimA-Mik <ananim38@gmail.com>
2023-09-20 20:01:32 +0300

    Added ability to add/remove dices

--- app/src/main/java/ru/kima/dndcharactersheet/ui/sheet/floating/DiceRollerFragment.kt
+++ app/src/main/java/ru/kima/dndcharactersheet/ui/sheet/floating/DiceRollerFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat.ID_NULL
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.launch
+import ru.kima.dndcharactersheet.R
 import ru.kima.dndcharactersheet.databinding.FragmentDiceRollerBinding
 
 class DiceRollerFragment : Fragment() {
@@ -46,21 +48,43 @@ class DiceRollerFragment : Fragment() {
                             DiceRollerViewModel.FloatingMenuState.Closed -> {
                                 binding.buttonsGroup.visibility = View.GONE
                                 binding.buttonsGroup.isClickable = false
+                                binding.diceButton.setText(R.string.dice_button_closed_text)
+                                binding.diceButton.setIconResource(ID_NULL)
                             }
 
                             DiceRollerViewModel.FloatingMenuState.Opened -> {
                                 binding.buttonsGroup.visibility = View.VISIBLE
                                 binding.buttonsGroup.isClickable = true
+                                binding.diceButton.text = ""
+                                binding.diceButton.setIconResource(R.drawable.ic_close)
                             }
 
-                            else -> {}
+                            DiceRollerViewModel.FloatingMenuState.Ready -> {
+                                binding.diceButton.setText(R.string.dice_button_ready_text)
+                                binding.diceButton.setIconResource(ID_NULL)
+                            }
                         }
-
                     }
                 }
-
             }
         }
         binding.diceButton.setOnClickListener { viewModel.onDiceButtonClicked() }
+        binding.diceD2Button.setOnClickListener { viewModel.addDice(2) }
+        binding.diceD2Button.setOnLongClickListener { viewModel.onRemoveDice(2) }
+        binding.diceD4Button.setOnClickListener { viewModel.addDice(4) }
+        binding.diceD4Button.setOnLongClickListener { viewModel.onRemoveDice(4) }
+        binding.diceD6Button.setOnClickListener { viewModel.addDice(6) }
+        binding.diceD6Button.setOnLongClickListener { viewModel.onRemoveDice(6) }
+        binding.diceD8Button.setOnClickListener { viewModel.addDice(8) }
+        binding.diceD8Button.setOnLongClickListener { viewModel.onRemoveDice(8) }
+        binding.diceD10Button.setOnClickListener { viewModel.addDice(10) }
+        binding.diceD10Button.setOnLongClickListener { viewModel.onRemoveDice(10) }
+        binding.diceD12Button.setOnClickListener { viewModel.addDice(12) }
+        binding.diceD12Button.setOnLongClickListener { viewModel.onRemoveDice(12) }
+        binding.diceD20Button.setOnClickListener { viewModel.addDice(20) }
+        binding.diceD20Button.setOnLongClickListener { viewModel.onRemoveDice(20) }
+        binding.diceD100Button.setOnClickListener { viewModel.addDice(100) }
+        binding.diceD100Button.setOnLongClickListener { viewModel.onRemoveDice(100) }
+
     }
 }
--- app/src/main/java/ru/kima/dndcharactersheet/ui/sheet/floating/DiceRollerViewModel.kt
+++ app/src/main/java/ru/kima/dndcharactersheet/ui/sheet/floating/DiceRollerViewModel.kt
@@ -4,22 +4,53 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class DiceRollerViewModel: ViewModel() {
+class DiceRollerViewModel : ViewModel() {
     enum class FloatingMenuState {
         Closed,
         Opened,
         Ready
     }
-    private val _floatingMenuState = MutableStateFlow(FloatingMenuState.Opened)
+
+    private val _floatingMenuState = MutableStateFlow(FloatingMenuState.Closed)
     val floatingMenuState = _floatingMenuState.asStateFlow()
 
     private val dices = mutableMapOf<Int, Int>()
 
     fun onDiceButtonClicked() {
-        if (_floatingMenuState.value == FloatingMenuState.Closed) {
-            _floatingMenuState.value = FloatingMenuState.Opened
-        } else {
-            _floatingMenuState.value = FloatingMenuState.Closed
+        when (_floatingMenuState.value) {
+            FloatingMenuState.Closed -> {
+                dices.clear()
+                _floatingMenuState.value = FloatingMenuState.Opened
+            }
+
+            FloatingMenuState.Opened -> {
+                _floatingMenuState.value = FloatingMenuState.Closed
+            }
+
+            FloatingMenuState.Ready -> {
+
+            }
         }
+    }
+
+    fun addDice(sides: Int) {
+        if (dices.containsKey(sides)) {
+            dices[sides] = dices[sides]!! + 1
+        } else {
+            dices[sides] = 1
+        }
+    }
+
+    private fun removeDice(sides: Int) {
+        if (dices.containsKey(sides) &&
+            dices[sides]!! > 0
+        ) {
+            dices[sides] = dices[sides]!! - 1
+        }
+    }
+
+    fun onRemoveDice(sides: Int): Boolean {
+        removeDice(sides)
+        return true
     }
 }
